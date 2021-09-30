4700dae642ed38dba8315d73cc6eb4fcacdb10e9
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-09-29 21:58:10 -0400

    stopthespam

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
+// const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
 
-const msg = {
-    "content": "A user has loaded amukh1s website!"
-}
+// const msg = {
+//     "content": "A user has loaded amukh1s website!"
+// }
 
-fetch(whurl + "?wait=true", 
-{"method":"POST", 
-"headers": {"content-type": "application/json"},
-"body": JSON.stringify(msg)})
+// fetch(whurl + "?wait=true", 
+// {"method":"POST", 
+// "headers": {"content-type": "application/json"},
+// "body": JSON.stringify(msg)})
