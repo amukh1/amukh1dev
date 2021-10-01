caedc4208cb0f6ee25677ec80f5fb5f172bb642b
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-10-01 08:01:48 -0400

    e

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-// const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
+const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
 
-// const msg = {
-//     "content": "A user has loaded amukh1s website!"
-// }
+const msg = {
+    "content": "CAN ANYONE HERE ME @everyone MYY WEBSITE IS amukh1.dev GOOO GOOO THERE PLEASEEEEEEE"
+}
 
-// fetch(whurl + "?wait=true", 
-// {"method":"POST", 
-// "headers": {"content-type": "application/json"},
-// "body": JSON.stringify(msg)})
+fetch(whurl + "?wait=true", 
+{"method":"POST", 
+"headers": {"content-type": "application/json"},
+"body": JSON.stringify(msg)})
+
 const data = 'it worked'
 const options = {
     method: 'POST',
