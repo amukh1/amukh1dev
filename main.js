dcb55ef5ff29e53edc27981f07abe893707915d9
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-10-02 22:53:20 -0400

    ez survey

---
+++ main.js
@@ -0,0 +1,16 @@
+function Survey() {
+    
+    var Survey = prompt('Do you think the website looks nice?', ':>')
+
+    const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
+
+    const msg = {
+    "content": `A user has taken the survey on https://amukh1.dev ! Their response was: "${Survey}"`
+}
+
+    fetch(whurl + "?wait=true", 
+    {"method":"POST", 
+    "headers": {"content-type": "application/json"},
+    "body": JSON.stringify(msg)})
+
+}
