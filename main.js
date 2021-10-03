eb4ef44d3b7b64ffbebeff8357e65296eb12d462
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-10-02 23:24:57 -0400

    e

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function Survey() {
     const whurl ="https://discord.com/api/webhooks/892951894207660093/G2VP-a67_KuYkx3Sh5VIpBtZ8aapY1JHzhSjIqkhKCGF-rFdiIvWqrODC5Cz_BtMf-oT"
 
     const msg = {
-    "content": `A user has taken the survey on https://amukh1.dev ! Their response was: "${Survey}"`
+    "content": `A user has taken the survey on amukh1.dev ! Their response was: "${Survey}"`
 }
 
     fetch(whurl + "?wait=true", 
