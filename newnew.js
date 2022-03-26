3ba7bec3dca85d1d19be8dc1a1172490791628e1
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-03-26 14:48:14 -0400

    xx

--- newnew.js
+++ newnew.js
@@ -14,3 +14,17 @@ fetch(whurl + "?wait=true",
 {"method":"POST", 
 "headers": {"content-type": "application/json"},
 "body": JSON.stringify(msg)})
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+let data = JSON.parse(xhttp.responseText)
+      console.log(data)
+document.getElementById("ghrepos").innerHTML = data.public_repos
+    }
+};
+xhttp.open("GET", "https://api.github.com/users/amukh1", true);
+xhttp.send();
+
