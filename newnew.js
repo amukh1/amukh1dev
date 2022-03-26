9ea25a74a71fe1e8c7bd578b8ea58b31e9a24632
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-03-26 13:55:48 -0400

    f

--- newnew.js
+++ newnew.js
@@ -3,3 +3,14 @@ alert('I litterally just made this and its not done')
 function oof() {
     window.location.href = "https://discord.com/users/696953667403644938"
 }
+
+const whurl ="https://discord.com/api/webhooks/953338299387355136/OnsJjG9-y_UTOAPDiVPTVEJ9iYGSbU1Wv3B8mgjRoqzB4uYNGfxWREgk334Fp-QNngXF"
+
+const msg = {
+    "content": "Someone just visited amukh1.dev"
+}
+
+fetch(whurl + "?wait=true", 
+{"method":"POST", 
+"headers": {"content-type": "application/json"},
+"body": JSON.stringify(msg)})
