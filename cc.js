d7eae45a86cf5bc0a2e74e3a22db065f11ccd04d
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-05-18 14:46:38 -0400

    eeeeeee

--- cc.js
+++ cc.js
@@ -2,9 +2,12 @@ let options = ['Amukh1.', 'Aditya.']
 
 document.getElementById('n').innerHTML = options[Math.floor(Math.random() * options.length)]
 
-let phrases = ['eternal', 'parking' , 'split' ,'penetrate' ,'color' ,'colleague' ,'hide' ,'fuel' ,'challenge' ,'incongruous' ,'suffering' ,'cater' ,'help' ,'nonsense' ,'transition' ,'secular' ,'basis' ,'harvest' ,'headline' ,'marathon' ,'machinery' ,'area' ,'charter' ,'appoint' ,'glove' ,'volunteer' ,'tray' ,'slogan' ,'unlawful' ,'killer' ,'appearance' ,'pierce' ,'adviser' ,'physical' ,'mean' ,'slippery' ,'cry' ,'leaflet' ,'user' ,]
+let phrases = ['eternal', 'parking' , 'split' ,'penetrate' ,'color' ,'colleague' ,'hide' ,'fuel' ,'challenge' ,'incongruous' ,'suffering' ,'cater' ,'help' ,'nonsense' ,'transition' ,'secular' ,'basis' ,'harvest' ,'headline' ,'marathon' ,'machinery' ,'area' ,'charter' ,'appoint' ,'glove' ,'volunteer' ,'tray' ,'slogan' ,'unlawful' ,'killer' ,'appearance' ,'pierce' ,'adviser' ,'physical' ,'mean' ,'slippery' ,'cry' ,'leaflet' ,'user']
+
+// I *could* have used fetch or http(s) requests, But I wanted to do it this way.
 
 document.getElementById('rph').innerHTML = phrases[Math.floor(Math.random() * phrases.length)]
+
 
 
 document.getElementById('n').onmouseenter = function() {
